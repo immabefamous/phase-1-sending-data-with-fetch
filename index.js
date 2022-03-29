4261b95d869cc2e92287a51aa96f490c63ffebc0
Emmanuel <emmanuelbboatteng@yahoo.com>
2022-03-29 12:06:09 -0400

    commit tree bb6c8e555b829555b34b35c1c7cf87bd99528b85

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+const submitData = (name, email) => {
+      const formData = {
+          name: name,
+          email: email
+        }
+        
+        const configurationObject = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(formData),
+        };
+    fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        console.log(object)
+        document.body.append(object.id)
+      })
+      .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        document.body.append('Unauthrorized Access')
+      });
+  }
+  submitData() 
